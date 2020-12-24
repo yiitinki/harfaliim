60db1cb962eea514b9a2a7569dc3cd94bc022c0a
Yigit Agca <yigit@agca.me>
2020-12-24 02:35:29 +0200

    Full height screen problem has been solved

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="hello">
-    <img src="../assets/logo.png" width="90%" height="100%">
+    <div>
+        <img src="../assets/logo.png" width="90%" height="100%">
+    </div>
     <div class="question">{{description}}</div>
 
     <div class="sucess" v-if="answer.join('').toLowerCase() == words[0].toLowerCase()"> Tebrikler! </div>
 
     <ul>
         <li v-for="(char,i) of words[0]" :key="i">
-            <input :ref="'input'" v-if="chars.indexOf(i) != -1" :value="char" maxlength="1">
-            <input :ref="'input'" v-else v-model="answer[i]" maxlength="1" @keyup="nextChar(i)" >
+            <input :ref="'input'" v-if="chars.indexOf(i) != -1" :value="char" maxlength="1" disabled>
+            <input :ref="'input'" v-else v-model="answer[i]" maxlength="1" @keyup="nextChar(i)" disabled>
         </li>
     </ul>
 
-    <button class="char" @click="getChar()" v-show="answer.join('').length != words[0].length"> Harf Aliim </button>
-    <p></p>
-    <button class="next" @click="clearAnswer()"> Cevap Alanını Temizle </button>
-    <button class="next" @click="nextWord()"> Sonraki Soru</button>
+    <div style="display:flex;flex-direction:column;margin:5px;">
+        <button class="char" @click="getChar()" v-show="answer.join('').length != words[0].length"> Harf Aliim </button>
+
+        <button class="next" @click="clearAnswer()"> Cevap Alanını Temizle </button>
+        <button class="next" @click="nextWord()"> Sonraki Soru</button>
+    </div>
+
+    <div class="keyboard">
+        <p>
+            <button class="letter" @click="type(letter)" v-for="(letter,i) of keyboard[0]" :key="i">{{letter.toUpperCase()}}</button>
+        </p>
+        <p>
+            <button class="letter" @click="type(letter)" v-for="(letter,i) of keyboard[1]" :key="i">{{ letter == "i" ? "İ" : letter.toUpperCase()}}</button>
+        </p>
+        <p>
+            <button class="letter" @click="type(letter)" v-for="(letter,i) of keyboard[2]" :key="i">{{letter.toUpperCase()}}</button>
+        </p>
+    </div>
   </div>
 </template>
 
@@ -29,7 +45,12 @@ export default {
           description: '',
           words: [],
           chars: [],
-          answer: []
+          answer: [],
+          keyboard: [
+              ["q","w","e","r","t","y","u","ı","o","p","ğ","ü"],
+              ["a","s","d","f","g","h","j","k","l","ş","i"],
+              ["z","x","c","v","b","n","m","ö","ç"],
+            ]
       }
   },
   async created() {
@@ -52,7 +73,7 @@ export default {
       nextWord(){
           this.clearAnswer();
           if(this.words.length != 1){
-              this.words.shift();
+            this.words.shift();
             this.getDescription();
           }else{
               this.getWords();
@@ -88,6 +109,17 @@ export default {
       clearAnswer(){
           this.answer = [];
           this.chars= [];
+      },
+
+      type(letter){
+        let nullIndex = this.focusAnswerBox()
+
+        nullIndex != -1 ? this.$set(this.answer, nullIndex, letter) : this.answer.push(letter);
+        
+      },
+
+      focusAnswerBox(){
+          return this.answer.findIndex(v => v == null);
       }
   }
 }
@@ -96,13 +128,17 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-h3 {
-    margin: 40px 0 0;
+.hello{
+    display: flex;
+    flex-direction: column;
+    justify-content:space-between;
+    height: 90%;
+    padding: 5% 0;
 }
+
 ul {
     list-style-type: none;
     padding: 0;
-    font-size:40px;
 }
 li {
     border-bottom:1px grey solid; 
@@ -118,20 +154,25 @@ input{
     width:20px;
     font-size:20px;
     text-transform:lowercase;
+    color: black;
+    text-align: center;
+    border: 1px black solid;
 }
-button{
+
+.char,.next{
     font-size:18px;
     border:1px #36729A solid;
     padding: 10px;
     margin:0 20px;
     text-align: center;
 }
-button:hover{
+.char:hover,.next:hover{
     cursor:pointer
 }
 
 .question{
-    margin: 50px 0;
+    margin: 25px 0;
+    font-size: 20px;
 }
 .char{
     background:#36729A;
@@ -144,4 +185,22 @@ button:hover{
     font-size:20px;
     color:green;
 }
+
+
+.keyboard p{
+    display: flex;
+    flex-direction: row;
+    margin: 5px 0;
+}
+
+.letter{
+    flex-grow:1;
+    width: 100%;
+    height: 100%;
+    font-size:18px;
+    border:1px #36729A solid;
+    padding:10px 0;
+}
+
+
 </style>
